83f6d0be72a349aed34764f774d881c589a6f38e
Sigsign <sig@signote.cc>
2024-07-20 20:56:34 +0900

    Add require-dev: friendsofphp/php-cs-fixer

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
+
+$finder = (new Finder())
+    ->in([
+        __DIR__ . '/app',
+    ])
+    ->append([
+        __FILE__,
+    ]);
+
+return (new Config())
+    ->setParallelConfig(ParallelConfigFactory::detect())
+    ->setRules([
+        '@PER-CS2.0' => true,
+        'ordered_imports' => [
+            'imports_order' => ['class', 'function', 'const'],
+            'sort_algorithm' => 'alpha',
+        ],
+    ])
+    ->setFinder($finder);
