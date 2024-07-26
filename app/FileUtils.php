e40ca960a6348cd6bfbaff982bbb1d82dc155f2d
Sigsign <sig@signote.cc>
2024-07-27 01:04:07 +0900

    [WIP] Implement FileUtils

---
+++ app/FileUtils.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sigsign\IceMint;
+
+final class FileUtils
+{
+    private string $baseDir;
+
+    public function __construct(string $directory)
+    {
+        $dir = realpath($directory);
+        if (!$dir) {
+            throw new \InvalidArgumentException(
+                "Does not exist or normalization failed: {$directory}",
+            );
+        }
+        clearstatcache(true, $dir);
+        if (!is_dir($dir) || !is_readable($dir)) {
+            throw new \InvalidArgumentException(
+                "Either not a directory or not readable: {$dir}",
+            );
+        }
+
+        $this->baseDir = $dir;
+    }
+
+    public function read(string $filename): string {
+        if (!file_exists($filename)) {
+            throw new \RuntimeException(
+                "Does not exist: {$filename}"
+            );
+        }
+        return file_get_contents($filename);
+    }
+}
