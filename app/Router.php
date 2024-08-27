6a87a5bb0b55db78f67d1cfa07bc61e8bd539ad1
Sigsign <sig@signote.cc>
2024-08-27 10:59:24 +0900

    Implement RouterInterface

--- app/Router.php
+++ app/Router.php
@@ -5,13 +5,22 @@ declare(strict_types=1);
 namespace Sigsign\IceMint;
 
 use Sigsign\IceMint\Controller\ViewController;
+use Sigsign\IceMint\Routing\RouterInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Generator\UrlGenerator;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
 use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
 
-final class Router
+final class Router implements RouterInterface
 {
+    private ?UrlMatcher $matcher = null;
+    private ?UrlGenerator $generator = null;
+
     public function __construct(
         private ?RouteCollection $routes = null,
+        private RequestContext $context = new RequestContext(),
     ) {}
 
     public function routes(): RouteCollection
@@ -41,5 +50,62 @@ final class Router
         ], methods: [ 'POST' ]));
 
         return $routes;
+    }
+
+    public function getRouteCollection(): RouteCollection
+    {
+        return $this->routes();
+    }
+
+    public function getMatcher(): UrlMatcher
+    {
+        if ($this->matcher) {
+            return $this->matcher;
+        }
+
+        $routes = $this->getRouteCollection();
+        $this->matcher = new UrlMatcher($routes, $this->context);
+
+        return $this->matcher;
+    }
+
+    public function getGenerator(): UrlGenerator
+    {
+        if ($this->generator) {
+            return $this->generator;
+        }
+
+        $routes = $this->getRouteCollection();
+        $this->generator = new UrlGenerator($routes, $this->context);
+
+        return $this->generator;
+    }
+
+    public function match(string $pathinfo): array
+    {
+        return $this->getMatcher()->match($pathinfo);
+    }
+
+    public function matchRequest(Request $request): array
+    {
+        return $this->getMatcher()->matchRequest($request);
+    }
+
+    public function generate(
+        string $name,
+        array $parameters = [],
+        int $referenceType = self::ABSOLUTE_PATH,
+    ): string {
+        return $this->getGenerator()->generate($name, $parameters, $referenceType);
+    }
+
+    public function getContext(): RequestContext
+    {
+        return $this->context;
+    }
+
+    public function setContext(RequestContext $context): void
+    {
+        $this->context = $context;
     }
 }
---
+++ tests/RouterTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use Sigsign\IceMint\Router;
+use Symfony\Component\HttpFoundation\Request;
+
+class RouterTest extends TestCase
+{
+    public function testMatch(): void
+    {
+        $router = new Router();
+        $this->assertArrayHasKey('_controller', $router->match('/'));
+    }
+
+    public function testMatchRequest(): void
+    {
+        $router = new Router();
+        $request = Request::create('/');
+        $this->assertArrayHasKey('_controller', $router->matchRequest($request));
+    }
+
+    public function testGenerate(): void
+    {
+        $router = new Router();
+        $this->assertEquals('/', $router->generate('top'));
+    }
+}
